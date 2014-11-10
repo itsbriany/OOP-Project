ad31cf17b4c064b01b767b387ae02350c27a0380
brendanbeerman <brendanbeerman@gmail.com>
2014-11-10 00:17:35 -0400

    newest
    Has a tab and search part feel free to edit it, only made one tab, since
    all the others will be c&p

---
+++ Project2/Project2/MyForm.h
@@ -0,0 +1,211 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+namespace Project2 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TabPage^  tabPage2;
+	protected:
+	private: System::Windows::Forms::TabControl^  tabControl1;
+	private: System::Windows::Forms::TabPage^  tabPage1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::DirectoryServices::DirectoryEntry^  directoryEntry1;
+	private: System::Windows::Forms::Label^  label2;
+
+
+
+	protected:
+
+
+
+
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
+			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->directoryEntry1 = (gcnew System::DirectoryServices::DirectoryEntry());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->tabPage2->SuspendLayout();
+			this->tabControl1->SuspendLayout();
+			this->tabPage1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// tabPage2
+			// 
+			this->tabPage2->Controls->Add(this->label2);
+			this->tabPage2->Controls->Add(this->textBox2);
+			this->tabPage2->Location = System::Drawing::Point(4, 22);
+			this->tabPage2->Name = L"tabPage2";
+			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage2->Size = System::Drawing::Size(357, 281);
+			this->tabPage2->TabIndex = 1;
+			this->tabPage2->Text = L"Images";
+			this->tabPage2->UseVisualStyleBackColor = true;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(26, 38);
+			this->textBox2->Multiline = true;
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(295, 216);
+			this->textBox2->TabIndex = 0;
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox2_TextChanged);
+			// 
+			// tabControl1
+			// 
+			this->tabControl1->Controls->Add(this->tabPage1);
+			this->tabControl1->Controls->Add(this->tabPage2);
+			this->tabControl1->Location = System::Drawing::Point(0, 1);
+			this->tabControl1->Name = L"tabControl1";
+			this->tabControl1->SelectedIndex = 0;
+			this->tabControl1->Size = System::Drawing::Size(365, 307);
+			this->tabControl1->TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this->tabPage1->Controls->Add(this->button1);
+			this->tabPage1->Controls->Add(this->label1);
+			this->tabPage1->Controls->Add(this->textBox1);
+			this->tabPage1->Location = System::Drawing::Point(4, 22);
+			this->tabPage1->Name = L"tabPage1";
+			this->tabPage1->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage1->Size = System::Drawing::Size(357, 281);
+			this->tabPage1->TabIndex = 0;
+			this->tabPage1->Text = L"Search Tab";
+			this->tabPage1->UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(130, 109);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"search";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(80, 67);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(166, 13);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Enter what you want to search for";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(83, 83);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(163, 20);
+			this->textBox1->TabIndex = 0;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(147, 12);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(42, 13);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Results";
+			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(366, 306);
+			this->Controls->Add(this->tabControl1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->tabPage2->ResumeLayout(false);
+			this->tabPage2->PerformLayout();
+			this->tabControl1->ResumeLayout(false);
+			this->tabPage1->ResumeLayout(false);
+			this->tabPage1->PerformLayout();
+			this->ResumeLayout(false);
+
+		}
+
+#pragma endregion
+
+private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {//label for search tab
+}
+
+private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {//textbox for search tab
+}
+//c&p this for other future tabs
+private: System::Void textBox2_TextChanged(System::Object^  sender, System::EventArgs^  e) {//textbox for images tab
+	
+	ifstream file(filename.txt);	//change filename to whatever connor will call it
+	string content, str;
+	int i=0;
+
+	for (getline(file, str)) {         // loop getting single characters
+		content += str;
+		content += '\n';
+	}
+
+	file.close();	// closes the file
+
+	textBox2->Text = content;	//should print out the content
+}
+private: System::Void label2_Click(System::Object^  sender, System::EventArgs^  e) {
+}
+};
+}
